d67d9c9065762d05ac941e2bb2387cc21426297f
JBZonda <jeroen.vanderborgh@gmail.com>
2023-08-10 13:45:06 +0200

    Make new project

---
+++ BornToMove/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BornToMove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
